1de89f36f4a06c4b9d0688629fe3674aaec68a38
Michael Caterisano <mcaterisano@gmail.com>
2017-04-25 14:22:25 -0400

    create Components folder

---
+++ src/Components/Triggers/Triggers.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton'
+import Widget from '../Widget/Widget'
+import './Triggers.css'
+
+class Triggers extends Component {
+    constructor() {
+        super();
+        this.state = {widgets: []}
+    }
+    add() {
+        const widgets = this.state.widgets
+        this.setState({
+            widgets: widgets.concat(<Widget />)
+        })
+    }
+    remove() {
+        const widgets = this.state.widgets
+        widgets.pop()
+        this.setState({
+            widgets: widgets
+        })
+    }
+    componentWillMount() {
+        console.log('will mount')
+    }
+    render() {
+        return (
+            <div>
+                <div className="triggers-wrapper">
+                    <div className="triggers-container">
+                        <RaisedButton label="add" onClick={this.add.bind(this)}/>
+                        <RaisedButton label="remove" onClick={this.remove.bind(this)} />
+                    </div>
+                </div>
+                <div className="widgets-container">
+                    {this.state.widgets.map(function(widget, index) {
+                        return widget
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Triggers;
---
+++ src/Components/Widget/Widget.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Widget.css';
+
+class Widget extends Component {
+    render() {
+        return (
+            <div className="widget">
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="red"></div>
+                <div className="blue"></div>
+            </div>
+        )
+    }
+}
+
+export default Widget
